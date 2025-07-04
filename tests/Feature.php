aa7bd056b03fa2a05af1896bc5cd7a24c48322c0
Patrik Strišovský <patrik.strisovsky7@gmail.com>
2025-07-04 20:57:05 +0200

    test: add `PdfTest` and remove `Feature` one

--- tests/Feature.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-it('foo', function (): void {
-    expect(true)->toBeTrue();
-});
---
+++ tests/PdfTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Patressz\LaravelPdf\Enums\Format;
+use Patressz\LaravelPdf\PdfBuilder;
+
+it('can instantiate the PdfBuilder', function () {
+    $pdfBuilder = PdfBuilder::create();
+
+    expect($pdfBuilder)->toBeInstanceOf(PdfBuilder::class);
+});
+
+it('can change format', function (string|Format $format) {
+    $pdfBuilder = PdfBuilder::create()
+        ->format($format);
+
+    expect($pdfBuilder->format)->toBe($format);
+})
+->with([
+    'A1',
+    Format::A5->value,
+]);
+
+it('can change `nodeBinaryPath`', function () {
+    $pdfBuilder = PdfBuilder::create()
+        ->setNodeBinaryPath('/usr/local/bin/node');
+
+    expect($pdfBuilder->nodeBinaryPath)->toBe('/usr/local/bin/node');
+});
+
+it('delete temporary files', function () {
+    $pdfBuilder = PdfBuilder::create();
+
+    // expect($pdfBuilder->tmpDirectory->exists())->toBeFalse();
+});
