232fb3b2f856a2c5168db58f98e2aa7ab664650d
Patrik Strišovský <patrik.strisovsky7@gmail.com>
2025-07-24 19:54:21 +0200

    refactor: use `Pdf` facade in tests instead of `PdfBuilder`

--- tests/PdfTest.php
+++ tests/PdfTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Patressz\LaravelPdf\Enums\Format;
 use Patressz\LaravelPdf\Enums\Unit;
+use Patressz\LaravelPdf\Facades\Pdf;
 use Patressz\LaravelPdf\PdfBuilder;
 
 it('can instantiate the PdfBuilder', function () {
@@ -13,8 +14,7 @@ it('can instantiate the PdfBuilder', function () {
 });
 
 it('can change format', function (string|Format $format) {
-    $pdfBuilder = PdfBuilder::create()
-        ->format($format);
+    $pdfBuilder = Pdf::format($format);
 
     expect($pdfBuilder->options['format'])->toBe($format);
 })
@@ -24,26 +24,23 @@ it('can change format', function (string|Format $format) {
     ]);
 
 it('throws exception for invalid format', function () {
-    PdfBuilder::create()->format('INVALID');
+    Pdf::format('INVALID');
 })->throws(InvalidArgumentException::class);
 
 it('can format with enum', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->format(Format::A6);
+    $pdfBuilder = Pdf::format(Format::A6);
 
     expect($pdfBuilder->options['format'])->toBe('A6');
 });
 
 it('can change orientation to landscape', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->landscape();
+    $pdfBuilder = Pdf::landscape();
 
     expect($pdfBuilder->options['landscape'])->toBeTrue();
 });
 
 it('can change width and height', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->width(210)
+    $pdfBuilder = Pdf::width(210)
         ->height(297);
 
     expect($pdfBuilder->options['width'])->toBe('210mm');
@@ -51,8 +48,7 @@ it('can change width and height', function () {
 });
 
 it('can change width and height in inches', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->width(8.27, Unit::Inch)
+    $pdfBuilder = Pdf::width(8.27, Unit::Inch)
         ->height(11.69, Unit::Inch);
 
     expect($pdfBuilder->options['width'])->toBe('8.27in');
@@ -60,89 +56,79 @@ it('can change width and height in inches', function () {
 });
 
 it('throws exception for invalid unit in width', function () {
-    PdfBuilder::create()->width(210, 'invalid');
+    Pdf::width(210, 'invalid');
 })->throws(InvalidArgumentException::class);
 
 it('throws exception for invalid unit in height', function () {
-    PdfBuilder::create()->height(297, 'invalid');
+    Pdf::height(297, 'invalid');
 })->throws(InvalidArgumentException::class);
 
 it('can set outline', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->outline();
+    $pdfBuilder = Pdf::outline();
 
     expect($pdfBuilder->options['outline'])->toBeTrue();
 });
 
 it('can set scale', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->scale(1.5);
+    $pdfBuilder = Pdf::scale(1.5);
 
     expect($pdfBuilder->options['scale'])->toBe(1.5);
 });
 
 it('throws exception for invalid scale', function (float $scale) {
-    PdfBuilder::create()->scale($scale);
+    Pdf::scale($scale);
 })
     ->with([0, 2.5])
     ->throws(InvalidArgumentException::class, 'Scale must be a positive number between 0.1 and 2.');
 
 it('can set print background', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->printBackground();
+    $pdfBuilder = Pdf::printBackground();
 
     expect($pdfBuilder->options['printBackground'])->toBeTrue();
 });
 
 it('can set tagged PDF', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->tagged();
+    $pdfBuilder = Pdf::tagged();
 
     expect($pdfBuilder->options['tagged'])->toBeTrue();
 });
 
 it('can set page ranges', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->pageRanges('1-5, 8, 11-13');
+    $pdfBuilder = Pdf::pageRanges('1-5, 8, 11-13');
 
     expect($pdfBuilder->options['pageRanges'])->toBe('1-5, 8, 11-13');
 });
 
 it('throws exception for empty page ranges', function () {
-    PdfBuilder::create()->pageRanges('');
+    Pdf::pageRanges('');
 })->throws(InvalidArgumentException::class, 'Page ranges cannot be empty.');
 
 it('can set prefer CSS page size', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->preferCSSPageSize();
+    $pdfBuilder = Pdf::preferCSSPageSize();
 
     expect($pdfBuilder->options['preferCSSPageSize'])->toBeTrue();
 });
 
 it('can set display header footer', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->displayHeaderFooter();
+    $pdfBuilder = Pdf::displayHeaderFooter();
 
     expect($pdfBuilder->options['displayHeaderFooter'])->toBeTrue();
 });
 
 it('can set header template', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->headerTemplate('<div>Header</div>');
+    $pdfBuilder = Pdf::headerTemplate('<div>Header</div>');
 
     expect($pdfBuilder->headerHtml)->toBe('<div>Header</div>');
 });
 
 it('can set footer template', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->footerTemplate('<div>Footer</div>');
+    $pdfBuilder = Pdf::footerTemplate('<div>Footer</div>');
 
     expect($pdfBuilder->footerHtml)->toBe('<div>Footer</div>');
 });
 
 it('can set header and footer templates with view', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->headerTemplate(view('header'))
+    $pdfBuilder = Pdf::headerTemplate(view('header'))
         ->footerTemplate(view('footer'));
 
     expect($pdfBuilder->headerHtml)->toBe(view('header')->render());
@@ -150,8 +136,7 @@ it('can set header and footer templates with view', function () {
 });
 
 it('can set margin', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->margins(10, 20, 30, 40);
+    $pdfBuilder = Pdf::margins(10, 20, 30, 40);
 
     expect($pdfBuilder->margins['top'])->toBe('10mm');
     expect($pdfBuilder->margins['right'])->toBe('20mm');
@@ -160,8 +145,7 @@ it('can set margin', function () {
 });
 
 it('can set margin in inches', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->margins(1, 2, 3, 4, Unit::Inch);
+    $pdfBuilder = Pdf::margins(1, 2, 3, 4, Unit::Inch);
 
     expect($pdfBuilder->margins['top'])->toBe('1in');
     expect($pdfBuilder->margins['right'])->toBe('2in');
@@ -170,64 +154,56 @@ it('can set margin in inches', function () {
 });
 
 it('can set download filename', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->name('invoice.pdf');
+    $pdfBuilder = Pdf::name('invoice.pdf');
 
     expect($pdfBuilder->downloadFileName)->toBe('invoice.pdf');
 });
 
 it('can set download filename with `name()` method over `download()` method `downloadFileName` argument', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->name('invoice-1.pdf')
+    $pdfBuilder = Pdf::name('invoice-1.pdf')
         ->download('invoice.pdf');
 
     expect($pdfBuilder->downloadFileName)->toBe('invoice-1.pdf');
 });
 
 it('can change `nodeBinaryPath`', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->setNodeBinaryPath('/usr/local/bin/node');
+    $pdfBuilder = Pdf::setNodeBinaryPath('/usr/local/bin/node');
 
     expect($pdfBuilder->nodeBinaryPath)->toBe('/usr/local/bin/node');
 });
 
 it('automatically adds pdf extension', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->name('invoice');
+    $pdfBuilder = Pdf::name('invoice');
 
     expect($pdfBuilder->downloadFileName)->toBe('invoice.pdf');
 });
 
 it('can add custom headers', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->addHeaders([
-            'X-Custom-Header' => 'test-value',
-            'Cache-Control' => 'no-cache',
-        ]);
+    $pdfBuilder = Pdf::addHeaders([
+        'X-Custom-Header' => 'test-value',
+        'Cache-Control' => 'no-cache',
+    ]);
 
     expect($pdfBuilder->responseHeaders['X-Custom-Header'])->toBe('test-value');
     expect($pdfBuilder->responseHeaders['Cache-Control'])->toBe('no-cache');
 });
 
 it('can set download headers', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->download('test.pdf');
+    $pdfBuilder = Pdf::download('test.pdf');
 
     expect($pdfBuilder->responseHeaders['Content-Type'])->toBe('application/pdf');
     expect($pdfBuilder->responseHeaders['Content-Disposition'])->toBe('attachment; filename="test.pdf"');
 });
 
 it('can set inline headers', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->inline('test.pdf');
+    $pdfBuilder = Pdf::inline('test.pdf');
 
     expect($pdfBuilder->responseHeaders['Content-Type'])->toBe('application/pdf');
     expect($pdfBuilder->responseHeaders['Content-Disposition'])->toBe('inline; filename="test.pdf"');
 });
 
 it('can set view', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->view('layout');
+    $pdfBuilder = Pdf::view('layout');
 
     $html = view('layout')->render();
 
@@ -235,15 +211,13 @@ it('can set view', function () {
 });
 
 it('can set html content', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->html('<h1>Hello World</h1>');
+    $pdfBuilder = Pdf::html('<h1>Hello World</h1>');
 
     expect($pdfBuilder->html)->toBe('<h1>Hello World</h1>');
 });
 
 it('can set html content using `html()` method over `view()` method', function () {
-    $pdfBuilder = PdfBuilder::create()
-        ->view('layout')
+    $pdfBuilder = Pdf::view('layout')
         ->html('<h1>Hello World</h1>');
 
     expect($pdfBuilder->html)->toBe('<h1>Hello World</h1>');
@@ -266,8 +240,7 @@ it('delete temporary files', function () {
 });
 
 it('can save PDF file with correct format', function () {
-    $path = PdfBuilder::create()
-        ->view('layout')
+    $path = Pdf::view('layout')
         ->format(Format::A2)
         ->save(getTempDir('test.pdf'));
 
@@ -278,8 +251,7 @@ it('can save PDF file with correct format', function () {
 });
 
 it('can save PDF file with correct format using `width()` and `height()` method', function () {
-    $path = PdfBuilder::create()
-        ->view('layout')
+    $path = Pdf::view('layout')
         ->width(1587, Unit::Pixel)
         ->height(2246, Unit::Pixel)
         ->save(getTempDir('test.pdf'));
@@ -291,8 +263,7 @@ it('can save PDF file with correct format using `width()` and `height()` method'
 });
 
 it('can save PDF file with landscape orientation', function () {
-    $path = PdfBuilder::create()
-        ->view('layout')
+    $path = Pdf::view('layout')
         ->landscape()
         ->save(getTempDir('test.pdf'));
 
